fd2d029b7f5cef2cdb4e8ffd77a29e359d3833e5
Marcello <marcicarboni00@gmail.com>
2021-09-23 11:29:59 +0200

    Creato backend, cambiata la navbar

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict';
+const express = require('express'),
+app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+
+// Test ports
+const PORT = 5000;
+const HOST = '0.0.0.0';
+
+// App
+app.get('/', (req, res) => {
+  res.redirect('index');
+});
+
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about-us', (req, res) => {
+    res.render('about-us');
+  });
+
+app.get('*', (req, res) => {
+  res.status(404).send("Not found.");
+})
+
+app.listen(PORT, HOST);
+console.log(" ________________");
+console.log("|                | ");
+console.log(`|  IP: ${HOST}   |`);
+console.log(`|   PORT: ${PORT}   |`);
+console.log("|＿＿＿＿＿＿＿＿|");
+console.log("(\\\__/) ||");
+console.log("(•ㅅ•) ||");
+console.log("/ 　 づ");
