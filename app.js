638a2b155b2f4b18cf052b5fcdee35b2034d2570
takenX10 <frau.alessandro01@gmail.com>
2021-10-20 13:00:37 +0200

    aggiunto manager
    Aggiunto manager per aggiunta e rimozione utenti e writeup, per ora configurata solo l'aggiunta di entrambi i campi tramite json
    TODO:
    - rimozione writeup ed utenti
    - lista writeup ed utenti
    - aggiunta immagini utenti
    - miglioramento grafico generale
    - parsare il writeup giusto a seconda della richiesta

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 'use strict';
 const express = require('express'),
 app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
+app.use(bodyParser.urlencoded({ extended: false }));
 const fs = require('fs')
 
 app.set("view engine", "ejs");
@@ -25,20 +28,51 @@ app.get('/writeups', (req, res) => {
 app.get('/about-us', (req, res) => {
     res.render('about-us');
 });
+// SrdnlenIsTheKing :)
+app.get('/e390cd59e40e7dc601c9a8c1cde91417e6cc18bd950f8f061fff24b1b23b81b6/manager', (req, res) => {
+  res.render('manager');
+});
 
-app.get('/parser', (req, res) => {
-  res.render('parser');
+app.get('/e390cd59e40e7dc601c9a8c1cde91417e6cc18bd950f8f061fff24b1b23b81b6/members-manager', (req, res) => {
+  res.render('members-manager');
+});
+
+app.get('/e390cd59e40e7dc601c9a8c1cde91417e6cc18bd950f8f061fff24b1b23b81b6/writeups-manager', (req, res) => {
+  res.render('writeups-manager');
+});
+
+app.post('/e390cd59e40e7dc601c9a8c1cde91417e6cc18bd950f8f061fff24b1b23b81b6/add-manager', (req, res) => {
+  var tipo = req.body['type'];
+  delete req.body.type
+  res.setHeader('Content-Type', 'application/json');
+  try{
+    if(tipo == 'members'){
+      const filecontent = fs.readFileSync('json/members.json', 'utf8');
+      var final_json = JSON.parse(filecontent);
+      final_json['members'].push(req.body);
+      fs.writeFileSync('json/members.json', JSON.stringify(final_json));
+      console.log(final_json);
+    }else if(tipo == 'writeup'){
+      const filecontent = fs.readFileSync('json/writeups.json', 'utf8');
+      var final_json = JSON.parse(filecontent);
+      final_json['writeups'].push(req.body);
+      fs.writeFileSync('json/writeups.json', JSON.stringify(final_json));
+    }
+    res.end(JSON.stringify({'value':'correct!'}));
+  }catch(errorValue){
+    res.end(JSON.stringify({'value':'ERROR!'}));
+  }
 });
 
 app.get('/writeup-1', (req, res) => {
   const data = fs.readFileSync('json/writeup.json', 'utf8');
-
   res.render('writeup', {content: escape(data)});
 });
 
 app.get('*', (req, res) => {
   res.status(404).send("Not found.");
 })
+
 
 app.listen(PORT, HOST);
 console.log(" ________________");
---
+++ public/scripts/manager.js
@@ -0,0 +1,8 @@
+
+
+function add_show(){
+    var b = document.getElementsByClassName('buttons-class');
+    b[0].style.display='none';
+    var a = document.getElementsByClassName('add');
+    a[0].style.display='';
+}
